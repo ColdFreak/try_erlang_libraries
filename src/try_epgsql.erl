72a1608185e77a38b280e4f62ebde8b390151b3e
Wang Zhijun <wang@gmail.com>
2015-08-30 23:12:14 +0900

    src/try_epgsql.erl

--- src/try_epgsql.erl
+++ src/try_epgsql.erl
@@ -3,14 +3,21 @@
 -export([func/0]).
 
 func() ->
+    %% ex1: つなげる
+    %% {ok, C} = epgsqla:start_link(),
+    %% Ref = epgsqla:connect(C, "localhost", "postgres", "", [{database, "elixir_china"}]),
+    %% receive 
+    %%     {C, Ref, connected} ->
+    %%         {ok, C};
+    %%     {C, Ref, Error = {error, _}} ->
+    %%         Error;
+    %%     {'EXIT', C, _Reason} ->
+    %%         {error, closed}
+    %% end.
+
+    
     {ok, C} = epgsqla:start_link(),
-    Ref = epgsqla:connect(C, "localhost", "postgres", "", [{database, "elixir_china"}]),
-    receive 
-        {C, Ref, connected} ->
-            {ok, C};
-        {C, Ref, Error = {error, _}} ->
-            Error;
-        {'EXIT', C, _Reason} ->
-            {error, closed}
-    end.
+    _Ref = epgsqla:connect(C, "localhost", "postgres", "", [{database, "elixir_china"}]),
+    SelectRes = epgsql:squery(C, "select count(*) from categories"),
+    io:format("~p~n", [SelectRes]).
 
